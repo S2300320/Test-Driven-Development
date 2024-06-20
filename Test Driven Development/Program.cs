d1e4ddd842cbc0fb0c43f1e8ae9ed28fcfaf403d
Alpha Bah <S2300320@ndonline.ac.uk>
2024-06-20 15:27:35 +0100

    added a new line on line 69

--- Test Driven Development/Program.cs
+++ Test Driven Development/Program.cs
@@ -66,7 +66,7 @@ namespace Test_Driven_Development
             }
             catch (Exception)
             {
-                Console.WriteLine("");
+                Console.WriteLine("no product found with id 0");
             }
 
         }
